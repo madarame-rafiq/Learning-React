16fbf887bbededabed99335ab7a767c676d8c9d7
Rafiq Mansuri <rafiqmansuri223@gmail.com>
2026-04-05 14:34:40 +0530

    Made some improvements

--- Day04-currencyConverter/src/App.jsx
+++ Day04-currencyConverter/src/App.jsx
@@ -13,10 +13,10 @@ function App(){
 
   const data = currencyInfo(from)
   
-  useEffect(()=>{
+  const currencyCalculate = ()=>{
     if(data[to])
       setConvertedAmount(amount * data[to])
-  },[amount, to, from, data]);
+  }
 
   const option = Object.keys(data)
 
@@ -28,12 +28,20 @@ function App(){
   return(
     <div className="container">
       <h1 id="heading">Currency Converter</h1>
+
+      <form className="container" onSubmit={(e)=>e.preventDefault()} >
+        <InputBox label="From" onChangeAmount={(am)=>setAmount(am)} disabled={false} options={option} onCurrencyChange={(curr) => setFrom(curr)} amount={amount} currency={from} />
+        
+        <button className="swapBtn" onClick={swap}>Swap</button>
+        
+        <InputBox label="To" disabled={true} options={option} onCurrencyChange={(curr) => setTo(curr)} amount={convertedAmount} currency={to} />
+
+        <button className="swapBtn" onClick={currencyCalculate}>Convert {from} to {to}</button>
+        
+
+
+      </form>
       
-      <InputBox label="From" onChangeAmount={(am)=>setAmount(am)} disabled={false} options={option} onCurrencyChange={(curr) => setFrom(curr)} amount={amount} calculate={()=>calCurrency()} currency={from} />
-      
-      <button id="swapBtn" onClick={swap}>Swap</button>
-      
-      <InputBox label="To" disabled={true} options={option} onCurrencyChange={(curr) => setTo(curr)} amount={convertedAmount} currency={to} />
     </div>
   )
 }
